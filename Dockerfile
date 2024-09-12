790123861907d0a481f742e72c54b4a86314a23f
EMMANUEL <50409804+emmy-1@users.noreply.github.com>
2024-09-12 16:13:07 +0100

    updated airflow dag, requirement.txt and docker compose file

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,10 @@
 FROM quay.io/astronomer/astro-runtime:12.1.0
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the requirements file
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
--- dags/data_pipline.py
+++ dags/data_pipline.py
@@ -1,2 +1,37 @@
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+from testscrapper import scrape_y_combinator, copy_to_snowflake
+
+
+
+def run_scraper():
+    url = "https://www.ycombinator.com/companies"
+    data_set = scrape_y_combinator(url)
+    return data_set
+
+def load_data(**kwargs):
+    ti = kwargs['ti']  # Get the task instance
+    df = ti.xcom_pull(task_ids='Extract_data')  # Pull the DataFrame from XCom
+    copy_to_snowflake(df)  # Pass the DataFrame to the copy function 
+
+
+
+with DAG(
+    'Y_combinator_extract_load',
+    default_args ={
+        "email": ['eobayomi2@gmail.com'],
+        "email_on_failure": True,
+        "email_on_retry": True,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    start_date = datetime(2023,1,1),
+    description = 'This Dag main goal is to extract data from the Y_combinator website and directly load the data into a snowflake database',
+    schedule ='@daily', catchup = False):
+
+    extract_load_data = PythonOperator(task_id = 'Extract_data', python_callable = run_scraper )
+    load_to_snowflake = PythonOperator(task_id= 'load_to_snowflake', python_callable = load_data, provide_context=True)
+
+    extract_load_data >> load_to_snowflake
+    
